7e2fc2c0af76cddac7a6a6d271a6258bd2b9a02d
Hemant Bavle <hemantbavle@gmail.com>
2025-06-28 10:05:19 +0530

    staged push

--- src/main/java/com/example/makemytrip/trains.java
+++ src/main/java/com/example/makemytrip/trains.java
@@ -1,4 +1,12 @@
 package com.example.makemytrip;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class trains {
+    @GetMapping("trains")
+    public String getData(){
+        return "Get train tkts faster!";
+    }
 }
